5e5a20b0a669faee7bd50b90ecb6550350185923
Royweru <royezi17@gmail.com>
2023-07-30 21:14:51 +0300

    new file:   .vscode/settings.json 	modified:   blog/__pycache__/admin.cpython-311.pyc 	modified:   blog/__pycache__/models.cpython-311.pyc 	modified:   blog/__pycache__/views.cpython-311.pyc 	modified:   blog/models.py 	modified:   blog/urls.py 	modified:   blog/views.py 	modified:   templates/Main.html 	new file:   templates/Navbar.html 	modified:   templates/base/Home.html 	new file:   templates/base/blogpage.html 	modified:   templates/base/newpost.html 	new file:   templates/comp/feeds.html 	new file:   templates/comp/topics.html

--- blog/urls.py
+++ blog/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from. import views
 urlpatterns = [
     path('',views.home_page, name='home'),
+    path('create-blog/', views.createBlog, name='create'),
+    
+    path('post/<str:pk>/', views.getblog, name='blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,30 @@
 from django.shortcuts import render,redirect
 
+from .models import User,Topic,Post
+from django.contrib import messages
 
 
 def home_page(request):
-    return render(request, 'base/Home.html')
+    posts = Post.objects.all()
+    topics = Topic.objects.all()
+    context = {'posts':posts,'topics':topics}
+    return render(request, 'base/Home.html', context)
 
 
+def getblog(request,pk):
+    post = Post.objects.get(id=pk)
+    context = {'post':post}
+    return render(request, 'base/blogpage.html',context)
+
+def createBlog(request):
+    topics = Topic.objects.all()
+    if request.method == 'POST':
+        Post.objects.create(
+            author = request.user,
+            title = request.POST['title'],
+            topic = request.POST['topic'],
+            body = request.POST['body']
+        )
+        return redirect('home')
+    context = {'topics':topics}
+    return render(request, 'base/newpost.html',context )
