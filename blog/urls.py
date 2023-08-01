aca6afec3262b964b8bf5c5d9b4afb51e7651117
Royweru <royezi17@gmail.com>
2023-08-01 13:33:57 +0300

    modified:   blog/__pycache__/__init__.cpython-311.pyc 	modified:   blog/__pycache__/models.cpython-311.pyc 	modified:   blog/__pycache__/urls.cpython-311.pyc 	modified:   blog/__pycache__/views.cpython-311.pyc 	modified:   blog/migrations/__pycache__/0001_initial.cpython-311.pyc 	modified:   blog/migrations/__pycache__/__init__.cpython-311.pyc 	modified:   blog/urls.py 	modified:   blog/views.py 	modified:   db.sqlite3 	modified:   templates/Navbar.html 	new file:   templates/base/blogtopic.html 	new file:   templates/base/userProfile.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,11 +3,13 @@ from. import views
 urlpatterns = [
     path('',views.home_page, name='home'),
     path('create-blog/', views.createBlog, name='create'),
+    path('user-profile/<str:pk>/', views.user_profile, name='user-profile'),
+    
+    path('topic/<str:pk>/', views.blogtopic, name='blogtopic'),
     
     path('sign-up/', views.sign_user, name='sign-up'),
     path('logout/', views.logout_user, name='logout'),
     path('login/', views.login_user, name='login' ),
-    
     
     path('post/<str:pk>/', views.getblog, name='blog'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import BlogForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 from .models import User, Topic, Post
 from django.contrib import messages
 
@@ -38,6 +39,12 @@ def logout_user(request):
     logout(request)
     return redirect('home')
 
+@login_required(login_url='login')
+def blogtopic(request,pk):
+    topic = Topic.objects.get(id=pk)
+    posts = topic.post_set.all()
+    context = {'posts':posts}
+    return render(request, 'base/blogtopic.html', context)
 
 def sign_user(request):
     if request.method == 'POST':
@@ -52,13 +59,13 @@ def sign_user(request):
         return redirect('home')
     return render(request, 'base/signup.html')
 
-
+@login_required(login_url='login')
 def getblog(request, pk):
     post = Post.objects.get(id=pk)
     context = {'post': post}
     return render(request, 'base/blogpage.html', context)
 
-
+@login_required(login_url='login')
 def createBlog(request):
     topics = Topic.objects.all()
     form = BlogForm()
@@ -72,3 +79,9 @@ def createBlog(request):
 
     context = {'topics': topics, 'form': form}
     return render(request, 'base/newpost.html', context)
+
+def user_profile(request,pk):
+    user = User.objects.get(id=pk)
+    posts = user.post_set.all()
+    context ={'posts':posts}
+    return render(request, 'base/userProfile', context)
