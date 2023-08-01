c815ce2408cf46aa8c51fd466191bec8c936cfb1
Royweru <royezi17@gmail.com>
2023-08-01 16:21:26 +0300

    modified:   blog/urls.py 	modified:   blog/views.py 	modified:   db.sqlite3 	modified:   templates/Navbar.html 	modified:   templates/base/Home.html 	new file:   templates/base/deleteblog.html 	new file:   templates/base/editblog.html

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,8 @@ from. import views
 urlpatterns = [
     path('',views.home_page, name='home'),
     path('create-blog/', views.createBlog, name='create'),
+    path('delete-post/<str:pk>/', views.delete_blog, name='delete-post'),
+    path('edit-post/<str:pk>/', views.update_blog, name='edit-post'),
     path('user-profile/<str:pk>/', views.user_profile, name='user-profile'),
     
     path('topic/<str:pk>/', views.blogtopic, name='blogtopic'),
--- blog/views.py
+++ blog/views.py
@@ -39,12 +39,14 @@ def logout_user(request):
     logout(request)
     return redirect('home')
 
+
 @login_required(login_url='login')
-def blogtopic(request,pk):
+def blogtopic(request, pk):
     topic = Topic.objects.get(id=pk)
     posts = topic.post_set.all()
-    context = {'posts':posts}
+    context = {'posts': posts}
     return render(request, 'base/blogtopic.html', context)
+
 
 def sign_user(request):
     if request.method == 'POST':
@@ -59,11 +61,13 @@ def sign_user(request):
         return redirect('home')
     return render(request, 'base/signup.html')
 
+
 @login_required(login_url='login')
 def getblog(request, pk):
     post = Post.objects.get(id=pk)
     context = {'post': post}
     return render(request, 'base/blogpage.html', context)
+
 
 @login_required(login_url='login')
 def createBlog(request):
@@ -80,8 +84,35 @@ def createBlog(request):
     context = {'topics': topics, 'form': form}
     return render(request, 'base/newpost.html', context)
 
-def user_profile(request,pk):
+
+def update_blog(request, pk):
+    blog = Post.objects.get(id=pk)
+    form = BlogForm(instance=blog)
+
+    if request.method == 'POST':
+        if request.user == blog.author:
+            form = BlogForm(request.POST, instance=blog)
+            if form.is_valid:
+                form.save()
+                messages.success(
+                    request, 'Your blog has been successfully updated👍')
+                return redirect('home')
+            else:
+                return messages.error(request, 'You are not allowed to delete the post of another author!!')
+    context = {'form': form}
+    return render(request, 'base/editblog.html', context)
+
+
+def delete_blog(request, pk):
+    post = Post.objects.get(id=pk)
+    if request.method == 'POST':
+        post.delete()
+
+    return render(request, 'base/deleteblog.html', {'post': post})
+
+
+def user_profile(request, pk):
     user = User.objects.get(id=pk)
     posts = user.post_set.all()
-    context ={'posts':posts}
+    context = {'posts': posts}
     return render(request, 'base/userProfile.html', context)
