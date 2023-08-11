e969cc71172fc3083d13796bea59590afcf1e8cd
Royweru <royezi17@gmail.com>
2023-08-11 17:32:31 +0300

    modified:   blog/__pycache__/models.cpython-311.pyc 	modified:   blog/__pycache__/urls.cpython-311.pyc 	modified:   blog/__pycache__/views.cpython-311.pyc 	new file:   blog/api/__pycache__/__init__.cpython-311.pyc 	new file:   blog/api/__pycache__/serializers.cpython-311.pyc 	new file:   blog/api/__pycache__/urls.cpython-311.pyc 	new file:   blog/api/__pycache__/views.cpython-311.pyc 	modified:   blog/api/serializers.py 	modified:   blog/api/views.py 	modified:   blogposts/__pycache__/settings.cpython-311.pyc 	modified:   blogposts/__pycache__/urls.cpython-311.pyc

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from blog.models import Post
+from .serializers import BlogSerializer
 @api_view(['GET'])
 def getBlogs(request):
     blogs  = Post.objects.all()
-    return Response(blogs)
+    serializer = BlogSerializer(blogs, many=True)
+    return Response(serializer.data)
