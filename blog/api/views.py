445c2c6d37e6190e4462f22c95463e988380f232
Royweru <royezi17@gmail.com>
2023-08-14 22:13:26 +0300

    modified:   blog/api/__pycache__/views.cpython-311.pyc 	modified:   blog/api/views.py

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,9 +1,19 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from blog.models import Post
+from rest_framework import status
 from .serializers import BlogSerializer
-@api_view(['GET'])
+@api_view(['GET','POST'])
 def getBlogs(request):
-    blogs  = Post.objects.all()
-    serializer = BlogSerializer(blogs, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        blogs  = Post.objects.all()
+        serializer = BlogSerializer(blogs, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = BlogSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    
