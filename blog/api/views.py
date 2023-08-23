891f1f4e2075889a16efba88cbd45fd141660a00
Royweru <royezi17@gmail.com>
2023-08-23 15:35:45 +0300

    modified:   blog/api/views.py

--- blog/api/views.py
+++ blog/api/views.py
@@ -15,5 +15,17 @@ def getBlogs(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        
-    
+@api_view(['PATCH','PUT','DELETE','GET'])        
+def blog_details(request,pk):
+    post = Post.objects.get(id=pk)
+    if request.method == 'PUT':
+        serializer = BlogSerializer(post,request.data)
+        if serializer.is_valid:
+            serializer.save()
+            return Response(serializer.data)
+    elif request.method == 'DELETE':
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    elif request.method == 'GET':
+        serializer = BlogSerializer(post)
+        return Response(serializer.data)
